8fbae684123e202bb8f23fa0453b1155baa3c198
juliusv <juliusvaliukas@gmail.com>
2018-11-30 19:36:14 +0200

    added asp net web api project

---
+++ flextech-web-api/flextech-web-api/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace flextech_web_api.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
