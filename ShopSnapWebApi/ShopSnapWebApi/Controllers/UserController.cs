2b0a97197d4d6a1d35620f19c2e1ed49777ac088
juliusv <juliusvaliukas@gmail.com>
2018-12-01 11:27:00 +0200

    Added data model

---
+++ ShopSnapWebApi/ShopSnapWebApi/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using ShopSnapWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShopSnapWebApi.Controllers
+{
+    public class UserController : ApiController
+    {
+        public List<User> GetUsers()
+        {
+            using(var db = new ShopSnapDatabaseContext())
+            {
+                return db.Users.ToList();
+            }
+        }
+    }
+}
