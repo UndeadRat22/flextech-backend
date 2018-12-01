60ae2704cd0d52b22b2e23c31b69dbe91deb53db
juliusv <juliusvaliukas@gmail.com>
2018-12-01 16:39:05 +0200

    Added get receipt items by user id

---
+++ ShopSnapWebApi/ShopSnapWebApi/Controllers/ReceiptItemController.cs
@@ -0,0 +1,45 @@
+﻿using ShopSnapWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShopSnapWebApi.Controllers
+{
+    public class ReceiptItemController : ApiController
+    {
+
+        public List<ReceiptItem> GetReceiptItemsByUserID(int userID)
+        {
+            using (var db = new ShopSnapDatabaseContext())
+            {
+                var userReceipts = db.Receipts.Include("ReceiptItems").Where(r => r.UserID == userID).ToList();
+
+                var returnItemList = new List<ReceiptItem>();
+
+                foreach (var receipt in userReceipts)
+                {
+                    var receiptItemList = receipt.ReceiptItems.ToList();
+
+                    foreach (var item in receiptItemList)
+                    {
+                        returnItemList.Add(
+                            new ReceiptItem()
+                            {
+                                ID = item.ID,
+                                Name = item.Name,
+                                Price = item.Price,
+                                Quantity = item.Quantity,
+                                ReceiptID = item.ReceiptID
+                            });
+
+                    }
+                }
+
+                return returnItemList;
+            }
+        }
+    }
+}
