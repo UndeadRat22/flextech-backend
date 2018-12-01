a603e59c70e03c4fa28c571c774183a556dbde7a
Faustas Butkus <faustas.butkus@gmail.com>
2018-12-01 18:51:24 +0200

    Replace corrupted value

---
+++ ShopSnapWebApi/ShopSnapWebApi/DataMaps/CorruptedWords.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShopSnapWebApi.DataMaps
+{
+    public static class WordMaps
+    {
+        private static Dictionary<string, string> words = new Dictionary<string, string>()
+        {
+            { "NIJOLAIDA", "NUOLAIDA" }
+        };
+
+        public static string ByKey(string key)
+        {
+            string value = null;
+
+            words.TryGetValue(key, out value);
+
+            return value;
+        }
+    }
+}
