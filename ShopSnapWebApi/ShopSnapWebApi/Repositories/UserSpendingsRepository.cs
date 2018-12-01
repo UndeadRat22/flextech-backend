ba8d845dd531bb90ce7bc7678d6b5905d1827899
Rolldicer <like.dmt@gmail.com>
2018-12-01 21:04:25 +0200

    Add month spendings

---
+++ ShopSnapWebApi/ShopSnapWebApi/Repositories/UserSpendingsRepository.cs
@@ -0,0 +1,50 @@
+﻿using ShopSnapWebApi.Controllers;
+using ShopSnapWebApi.Interfaces;
+using ShopSnapWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopSnapWebApi.Repositories
+{
+    public class UserSpendingsRepository : IUserSpendingsRepository
+    {
+
+        public decimal GetUserMonthSpendings(int userID)
+        {
+            decimal allSpendings = 0;
+            using (var db = new ShopSnapDatabaseContext())
+            {
+                var userReceipts = db.Receipts.Include("ReceiptItems").Where(r => r.UserID == userID).ToList();
+
+                var returnItemList = new List<ReceiptItem>();
+
+                foreach (var receipt in userReceipts)
+                {
+                    var receiptItemList = receipt.ReceiptItems.ToList();
+
+                    foreach (var item in receiptItemList)
+                    {
+                        //allSpendings += item.Quantity;
+                        returnItemList.Add(
+                            new ReceiptItem()
+                            {
+                                ID = item.ID,
+                                Name = item.Name,
+                                Price = item.Price,
+                                Quantity = item.Quantity,
+                                ReceiptID = item.ReceiptID
+                            });
+                        
+
+                    }
+                }
+
+                return allSpendings;
+            }
+        }
+
+
+    }
+}
