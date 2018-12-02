b119da74a1f80fec98bfb9878d3d16d06bf44f91
Valdas Rakutis <valdas@rakutis.lt>
2018-12-02 13:36:05 +0200

    lel

--- ShopSnapWebApi/ShopSnapWebApi/Controllers/ValuesController.cs
+++ ShopSnapWebApi/ShopSnapWebApi/Controllers/ValuesController.cs
@@ -31,8 +31,8 @@ namespace ShopSnapWebApi.Controllers
             List<FoundItem> found = null;
             var resp = await _service.GetStringFromImage(base64image);
             found = _parseService.GetItemList(resp);
-            if (found == null || found.Count < 3)
-                found = StaticReplyList.ItemList;
+            //if (found == null || found.Count < 3)
+            //    found = StaticReplyList.ItemList;
             return found;
         }
         
